1eab2c5129698fcf1bec50eeacab3e77f545774a
Caio Franco Gonçalves <101331197+caiofrancods@users.noreply.github.com>
2023-03-01 13:08:44 -0300

    Update device.component.ts

--- src/app/device/device.component.ts
+++ src/app/device/device.component.ts
@@ -125,7 +125,7 @@ export class DeviceComponent implements OnInit {
   async start() {
     await this.device.open();
     await this.device.selectConfiguration(1);
-    await this.device.descriptor.bNumInterfaces;
+    await console.log(this.device.deviceClass);
     await this.device.claimInterface(0);
    
 
